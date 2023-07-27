88d844b40006e77e3dcf289ce0201c97783e7099
Evgeny94 <lucky10291994@gmail.com>
2023-07-27 17:54:12 +0300

    Initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,20 @@
-import type { AppProps } from 'next/app'
-import React, {ReactElement, ReactNode, useState} from "react";
+import type {AppProps} from 'next/app'
+import React, {ReactElement, ReactNode} from "react";
 import {NextPage} from "next";
-import {Hydrate, QueryClient, QueryClientProvider} from "react-query";
 
 export type NextPageWithLayout<P = {}> = NextPage<P> & {
-  getLayout?: (page: ReactElement) => ReactNode;
+    getLayout?: (page: ReactElement) => ReactNode;
 };
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
+    Component: NextPageWithLayout;
 };
 
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
-  const [queryClient] = useState(() => new QueryClient)
+export default function App({Component, pageProps}: AppPropsWithLayout) {
 
-  const getLayout = Component.getLayout ?? ((page) => page);
+    const getLayout = Component.getLayout ?? ((page) => page);
 
-  return getLayout(
-      <QueryClientProvider client={queryClient}>
-        <Hydrate state={pageProps.dehydrateState}>
-          <Component {...pageProps} />
-        </Hydrate>
-      </QueryClientProvider>
-  );
+    return getLayout(
+        <Component {...pageProps} />
+    );
 }
