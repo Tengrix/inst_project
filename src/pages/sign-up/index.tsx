9c29b1aa5dd8acb1cb64640868fe59f834d19bb9
Evgeny94 <lucky10291994@gmail.com>
2023-07-28 16:34:12 +0300

    fix: routing names changed to kebab-case

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,10 +1,6 @@
 import {getLayout} from "src/components/Layout/BaseLayout/BaseLayout";
-import classes from "@/pages/sign-up/SignUp.module.css";
-import {yupResolver} from "@hookform/resolvers/yup";
-import Link from "next/link";
 import {useForm} from "react-hook-form";
 import {useSignUpMutation} from "api/authApi";
-import {signUpSchema} from "src/shared/utils/yupResolvers/yupResolver";
 import EmailSentModal from "src/styles/styledComponents/Modal/EmailSentModal";
 import {useState} from "react";
 import {Card} from "@/shared/ui/card";
@@ -16,7 +12,7 @@ import {registerSchema} from "@/shared/utils/schemas/register-schema";
 import {z} from "zod";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {ControlledTextField} from "@/shared/ui/controlled";
-import {log} from "next/dist/server/typescript/utils";
+import s from "./SignUp.module.scss"
 
 // export type SignUpFormData = {
 //     userName: string
@@ -32,9 +28,9 @@ type RegisterFormPropsType = {
 }
 
 const SignUp = () => {
-       const [showForgotModal, setShowForgotModal] = useState(false)
+    const [showForgotModal, setShowForgotModal] = useState(false)
     const [signUp] = useSignUpMutation()
-const onSubmitHandler=(data:RegisterFormType)=>console.log(data)
+    const onSubmitHandler = (data: RegisterFormType) => console.log(data)
     const {control, handleSubmit} = useForm<RegisterFormType>({resolver: zodResolver(registerSchema)})
     const onSubmit = handleSubmit(data => {
         console.log(data)
@@ -63,21 +59,24 @@ const onSubmitHandler=(data:RegisterFormType)=>console.log(data)
                 <form onSubmit={onSubmit}>
                     <ControlledTextField control={control} name={'userName'} label={'Username'} className={s.email}/>
                     <ControlledTextField control={control} name={'email'} label={'Email'} className={s.email}/>
-                    <ControlledTextField control={control} name={'password'} label={'Password'} className={s.password} type={'password'}
+                    <ControlledTextField control={control} name={'password'} label={'Password'} className={s.password}
+                                         type={'password'}
                     />
-                    <ControlledTextField control={control} name={'confirmPassword'} label={'Confirm password'} className={s.confirmPassword}
-                               type={'password'}
+                    <ControlledTextField control={control} name={'confirmPassword'} label={'Confirm password'}
+                                         className={s.confirmPassword}
+                                         type={'password'}
                     />
                     <Button type={'submit'} fullWidth className={s.registerBtn}>
                         Sign Up
                     </Button>
                 </form>
-                Do you have an account?
-                <Link href={'/sign-in'}>
-                    Sign in
-                </Link>
-            </div>
-
+                <Typography variant={'body2'} className={s.subtitle}>
+                    Do you have an account?
+                </Typography>
+                <Button as={'a'} variant={'link'} className={s.link} href={'/sign-in'}>
+                    Sign In
+                </Button>
+            </Card>
             <EmailSentModal handleClose={() => setShowForgotModal(false)} show={showForgotModal}/>
         </div>
     );
