e1171d28a8b4be9e8a5687eab111bb15a51fb644
2023git <samgold88@mail.ru>
2023-07-29 06:10:25 +0600

    Registration

--- api/apiSlice.ts
+++ api/apiSlice.ts
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ 
-    // baseUrl: 'https://instagramm-backend.vercel.app',
-    baseUrl: "https://forum-alpha.vercel.app/api",
+    // baseUrl: 'https://instagramm-backend.vercel.app',   // Ссылка на бэк стражеровки
+    baseUrl: "https://forum-alpha.vercel.app/api",         // Cсылка на мой бэк 
     prepareHeaders: (headers, { getState }) => {
       const token = localStorage.getItem('token');
       if (token) {
@@ -16,12 +16,13 @@ export const apiSlice = createApi({
   endpoints: (builder) => ({
     register: builder.mutation({
       query: (data) => ({
-
-        url: '/auth/register',
+        // url: '/auth/registration',                      // Endpoint на бэк стражеровки
+        url: '/auth/register',                             // Endpoint на мой бэк
         method: 'POST',
         body: data
       })
     }),
+
     registerEmailResending: builder.mutation({
       query: (data) => ({
         url: '/auth/registration-email-resending',
@@ -29,6 +30,7 @@ export const apiSlice = createApi({
         body: data
       })
     }),
+
     passwordRecovery: builder.mutation({
       query: (data) => ({
         url: '/auth/password-recovery',
@@ -36,6 +38,7 @@ export const apiSlice = createApi({
         body: data
       })
     }),
+
     newPassword: builder.mutation({
       query: (data) => ({
         url: '/auth/new-password',
@@ -43,6 +46,7 @@ export const apiSlice = createApi({
         body: data
       })
     }),
+
     login: builder.mutation({
       query: (credentials) => ({
         url: '/auth/login',
@@ -50,12 +54,14 @@ export const apiSlice = createApi({
         body: credentials
       })
     }),
+
     logout: builder.mutation({
       query: () => ({
         url: '/auth/logout',
         method: 'POST',
       })
     }),
+
     refreshToken: builder.mutation({
       query: () => ({
         url: '/auth/refresh-token',
@@ -66,7 +72,7 @@ export const apiSlice = createApi({
 })
 
 export const { 
-  useRegisterMutation, 
+  useRegisterMutation,  
   useRegisterEmailResendingMutation, 
   usePasswordRecoveryMutation, 
   useNewPasswordMutation, 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,3 @@
-// import '@/styles/globals.css'
-// import type { AppProps } from 'next/app'
-
-// export default function App({ Component, pageProps }: AppProps) {
-//   return <Component {...pageProps} />
-// }
-
-
-// _app.js
-// import { Provider } from 'react-redux';
-// import { store } from '../redux/store'; // import the store from your store file
-// import '../styles/globals.css';
-
-// function MyApp({ Component, pageProps }) {
-//   return (
-//     <Provider store={store}>
-//       <Component {...pageProps} />
-//     </Provider>
-//   )
-// }
-
-// export default MyApp;
-
-
 import { Provider } from 'react-redux';
 import { AppProps } from 'next/app';
 import { FC } from 'react'; // Импортируем тип FC (Functional Component) из библиотеки React
