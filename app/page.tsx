a2e396e604ec29a90955a5f561973a67d04cdba5
Evgeny94 <lucky10291994@gmail.com>
2023-07-31 18:25:01 +0300

    fix: git ignore

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import Image from 'next/image';
-import styles from './page.module.css';
-import SignUp from './signUp/page';
-import { Header } from '@/components/header/Header';
-
-export default function Home() {
-    return <SignUp />;
-}
