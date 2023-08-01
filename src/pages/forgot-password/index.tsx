35fe296a4a7b1f44541851c96eeeae49b322ee41
alexsharich <alexsharich@gmail.com>
2023-08-01 11:37:24 +0200

    feat: add forgotPassword to authApi, add forgotPasswordSchema, add add callback to captcha for button SEND LINK status(disabled) on forgotPassword page

--- src/pages/forgot-password/index.tsx
+++ src/pages/forgot-password/index.tsx
@@ -1,8 +1,5 @@
 import { getLayout } from 'src/components/Layout/BaseLayout/BaseLayout';
 import { useForm } from 'react-hook-form';
-import { useSignUpMutation } from 'api/authApi';
-import EmailSentModal from 'src/styles/styledComponents/Modal/EmailSentModal';
-import { useState } from 'react';
 import { Card } from '@/shared/ui/card';
 import { Typography } from '@/shared/ui/typography';
 import { Button } from '@/shared/ui/button';
@@ -11,29 +8,37 @@ import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { ControlledTextField } from '@/shared/ui/controlled';
 import s from './ForgotPassword.module.css';
-import { TextArea } from '@/shared/ui/text-area';
 import { Captcha } from '@/shared/captcha/Captcha';
-import { useRouter } from 'next/router';
-import { en } from 'locales/en';
-import { ru } from 'locales/ru';
 import { useTranslation } from './hooks/useTranslation';
+import { useForgotPasswordMutation } from '@/api/authApi';
+import { forgotPasswordSchema } from '@/shared/utils/schemas/forgotPasswordSchema';
+import { useState } from 'react';
 
-export type RegisterFormType = z.infer<typeof registerSchema>;
-type RegisterFormPropsType = {
+export type ForgotPasswordFormType = z.infer<typeof forgotPasswordSchema>;
+type ForgotPasswordFormPropsType = {
   linkPath: string;
-  onSubmitHandler: (data: RegisterFormType) => void;
+  onSubmitHandler: (data: ForgotPasswordFormType) => void;
 };
 
 const ForgotPassword = () => {
-  /*   const [showForgotModal, setShowForgotModal] = useState(false);
-  const [signUp] = useSignUpMutation();
-  const onSubmitHandler = (data: RegisterFormType) => console.log(data); */
-  const { control, handleSubmit } = useForm<RegisterFormType>({
-    resolver: zodResolver(registerSchema),
+  const [forgotPassword] = useForgotPasswordMutation();
+  const onSubmitHandler = (data: ForgotPasswordFormType) => console.log(data);
+
+  const { control, handleSubmit } = useForm<ForgotPasswordFormType>({
+    resolver: zodResolver(forgotPasswordSchema),
   });
-  const onSubmit = handleSubmit((data) => {});
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+    //onSubmitHandler(data); ???
+    forgotPassword(data);
+  });
 
   const { t } = useTranslation();
+  //
+  const [buttonSendLinkDisabled, setIsButtonSendLinkDisabled] = useState(true);
+  const changeCaptchaValue = (captchIsDone: boolean) => {
+    setIsButtonSendLinkDisabled(captchIsDone);
+  };
 
   return (
     <>
@@ -50,15 +55,19 @@ const ForgotPassword = () => {
             <Typography variant={'body2'} className={s.subtitle}>
               {t.auth.forgotPasswordPage.enterYourEmailText}
             </Typography>
-            <Button type={'submit'} fullWidth className={s.registerBtn}>
+            <Button
+              type={'submit'}
+              fullWidth
+              className={s.registerBtn}
+              disabled={buttonSendLinkDisabled}
+            >
               {t.auth.form.button.sendLink}
             </Button>
           </form>
           <Button as={'a'} variant={'link'} className={s.link} href={'/sign-in'}>
             {t.auth.form.button.backToSignIn}
           </Button>
-          <Captcha />
-          {/* TODO */}
+          <Captcha changeCaptchaValue={changeCaptchaValue} />
         </Card>
       </div>
     </>
--- src/shared/captcha/Captcha.tsx
+++ src/shared/captcha/Captcha.tsx
@@ -2,12 +2,17 @@ import { useState } from 'react';
 import ReCAPTCHA from 'react-google-recaptcha';
 import s from './Captcha.module.scss';
 
-export const Captcha = () => {
+type CaptchaPropsType = {
+  changeCaptchaValue: (captchIsDone: boolean) => void;
+};
+
+export const Captcha = ({ changeCaptchaValue }: CaptchaPropsType) => {
   const [captchIsDone, setCaptchaIsDone] = useState(false);
   const key = process.env.NEXT_PUBLIC_RECAPTCHA_API_KEY as string;
 
   const onChange = () => {
     setCaptchaIsDone(true);
+    changeCaptchaValue(captchIsDone);
   };
 
   return (
