10ff292ee40634bb0abf198e45a802af81f8147e
Evgeny94 <lucky10291994@gmail.com>
2023-08-01 15:56:33 +0300

    fix: LangSwitcher now display correct language name(english instead of en)

--- src/components/langSwitcher/LangSwitcher.tsx
+++ src/components/langSwitcher/LangSwitcher.tsx
@@ -5,16 +5,23 @@ import { ChangeEvent } from 'react';
 export const LangSwitcher = () => {
   const { locale, push, pathname, query, asPath, locales } = useRouter();
 
+  type languagesType = {
+      [language:string] : string
+  }
+  const languages:languagesType ={
+      'русский': 'ru',
+      'english': 'en'
+  }
   const changeLangHandler = (item: string) => {
     //const locale = event.currentTarget.value;
-    push({ pathname, query }, asPath, { locale: item });
+    push({ pathname, query }, asPath, { locale: languages[item]});
   };
 
   return (
     <Select
       items={[
-        { title: 'ru', icon: '' },
-        { title: 'en', icon: '' },
+        { title: 'Русский', icon: '' },
+        { title: 'English', icon: '' },
       ]}
       onValueChange={changeLangHandler}
     />
