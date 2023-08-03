28f3846e2499a1c09888a52b20c32c7c39c045c5
pavlik <readystores.ru@gmail.com>
2023-08-03 13:29:11 +0300

    feat: add Inter font family

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,17 @@ import '@/styles/variables/index.scss';
 import { NextPage } from 'next';
 import { NextIntlClientProvider } from 'next-intl';
 import type { AppProps } from 'next/app';
+import { Inter } from 'next/font/google';
 import { ReactElement, ReactNode } from 'react';
 import { Provider } from "react-redux";
 
+
+const inter = Inter({
+    display: 'swap',
+    style: ['normal'],
+    subsets: ['latin', 'cyrillic'],
+    weight: ['400', '500', '600', '700']
+})
 
 export type NextPageWithLayout<P = {}> = NextPage<P> & {
     getLayout?: (page: ReactElement) => ReactNode;
@@ -21,6 +29,12 @@ export default function App({Component, pageProps}: AppPropsWithLayout) {
     return getLayout(
         <Provider store={store}>
             <NextIntlClientProvider messages={pageProps.messages}>
+                <style jsx global>{`
+                    :root {--font-family-main: ${inter.style.fontFamily}, sans-serif}
+                    body {
+                        font-family: var(--font-family-main)
+                    }
+                `}</style>
                 <Component {...pageProps} />
             </NextIntlClientProvider>
         </Provider>
