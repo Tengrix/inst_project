166c98d7156d6badaac7cf83095161d61ab14780
2023git <samgold88@mail.ru>
2023-08-04 17:10:49 +0600

    fixing

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,47 +1,38 @@
-import Link from 'next/link';
-import classes from '@/components/Header/Header.module.css';
+import React, { ReactNode, ElementType, ComponentPropsWithoutRef } from 'react';
+import styles from './styles.module.css';
 import { LangSwitcher } from '../langSwitcher/LangSwitcher';
 
-const Header = () => (
-  <div className={classes.header}>
-    <div className={classes.container}>
-      <h2>Inctagram</h2>
+export type HeaderProps<T extends ElementType = 'header'> = {
+  title?: string,
+  children?: ReactNode,
+  icon?: ReactNode,
+  LanguageSwitcher?: ElementType,
+} & ComponentPropsWithoutRef<T>
 
-      <Link href={'/sign-in'}>
-        <h2>Sign In</h2>
-      </Link>
+const Header = <T extends ElementType = 'header'>(
+  props: HeaderProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof HeaderProps<T>>
+) => {
+  const {
+    title='Inctagram',
+    children,
+    icon = <img src={"/assets/notification.png"} alt="notification" />,
+    LanguageSwitcher = LangSwitcher,
+    ...rest
+  } = props;
 
-      <Link href={'/sign-up'}>
-        <h2>Sign Up</h2>
-      </Link>
-
-      <Link href={'/forgot-password'}>
-        <h2>Forgot Password</h2>
-      </Link>
-
-      <Link href={'/forgot-password/link-has-been-sent'}>
-        <h2>Forgot Password/Link sent</h2>
-      </Link>
-
-      <Link href={'/forgot-password/email-verification-link-expired'}>
-        <h2>Forgot Password/Link expired</h2>
-      </Link>
-
-      <Link href={'/forgot-password/create-new-password'}>
-        <h2>Create new password</h2>
-      </Link>
-
-      <Link href={'/sign-up/email-confirmed'}>
-        <h2>Email confirmation</h2>
-      </Link>
-
-      <Link href={'/sign-up/email-verification-link-expired'}>
-        <h2>Email Verification Link Expired</h2>
-      </Link>
-
-      <LangSwitcher />
-    </div>
-  </div>
-);
+  return (
+    <header className={styles.header} {...rest}>
+      <div className={styles.text}>
+        <h1 className={styles.inctagramTitle}> {title} </h1>
+      </div>
+      <div className={styles.languageSelector}>
+        <div className={styles.notificationIcon}>
+          {icon}
+        </div>
+        {children ? children : <LanguageSwitcher />}
+      </div>
+    </header>
+  );
+};
 
 export default Header;
