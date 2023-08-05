89ff61ff13c100549fd4c27cfaa3cd0302650e94
alexsharich <alexsharich@gmail.com>
2023-08-05 12:36:15 +0200

    feat: add placeholder components

--- src/pages/add-post/index.tsx
+++ src/pages/add-post/index.tsx
@@ -1,21 +1,23 @@
-import { getLayout } from "@/components/Layout/BaseLayout/BaseLayout";
-import { Modal } from "@/shared/ui/modal/Modal";
-import { Typography } from "@/shared/ui/typography";
+import { getLayout } from '@/components/Layout/BaseLayout/BaseLayout';
+import { Modal } from '@/shared/ui/modal/Modal';
+import { Typography } from '@/shared/ui/typography';
 import classes from './AddPost.module.scss';
-
+import { ImagePlaceholder, LoremIpsumPlaceholder } from '@/shared/ui/placeholder/placeholder';
 
 export const AddPost = () => {
+  return (
+    <div className={classes.container}>
+      <Typography as="h1" variant="h1">
+        Add Post
+      </Typography>
 
-    return (
-        <div className={classes.container}>
-            <Typography as="h1" variant="h1">Add Post</Typography>
-
-            <Modal open title="Add Photo" onClose={() => {}}>
-                <Typography as="p" variant="regular16">Content</Typography>
-            </Modal>
-        </div >
-    )
-}
+      <Modal open title="Add Photo" onClose={() => {}}>
+        <ImagePlaceholder />
+        <LoremIpsumPlaceholder />
+      </Modal>
+    </div>
+  );
+};
 
 AddPost.getLayout = getLayout;
 export default AddPost;
