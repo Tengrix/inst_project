c8b57fce9b94686f3195a3e59a1d2edfa107736e
pavlik <readystores.ru@gmail.com>
2023-08-06 16:40:02 +0300

    feat(add-page): add uploaded images preview

--- src/pages/add-post/index.tsx
+++ src/pages/add-post/index.tsx
@@ -5,20 +5,32 @@ import classes from './AddPost.module.scss';
 import { ImagePlaceholder, LoremIpsumPlaceholder } from '@/shared/ui/placeholder/placeholder';
 import { ImageUploader } from '@/shared/ui/file-uploader/file-uploader';
 import { useState } from 'react';
-import { addImage } from '@/shared/lib/imageStore';
+import { addImage, removeImage } from '@/shared/lib/imageStore';
 import { useDispatch } from 'react-redux';
+import { Button } from '@/shared/ui/button';
 import { useAppSelector } from '@/store';
+
 
 export const AddPost = () => {
   const [image, setImage] = useState<string>('');
   const dispatch = useDispatch();
+  const error = useAppSelector<string>(state => state.images.error);
+  const images = useAppSelector(state => state.images.images);
 
   const onImageChangeHandler = (event: any) => {
     if (event.target.files && event.target.files[0]) {
-      setImage(URL.createObjectURL(event.target.files[0]));
-      dispatch(addImage({ image: URL.createObjectURL(event.target.files[0]) }));
+      const { name, size, type } = event.target.files[0];
+      const src = URL.createObjectURL(event.target.files[0]);
+      setImage(src);
+      dispatch(addImage({ name, size, type, src }));
     }
   };
+
+  const deleteImageHandler = (event: any) => {
+        if (event.target.value) {
+            dispatch(removeImage({ src: event.target.value }));
+        }
+    };
 
   return (
     <div className={classes.container}>
@@ -26,9 +38,20 @@ export const AddPost = () => {
         Add Post
       </Typography>
       <div className={classes.placeholder}>
-        {!image ? <ImagePlaceholder /> : <img src={image} />}
+        {!image ? <ImagePlaceholder /> : <img src={image} alt="" />}
       </div>
+          {"Error " + error}
       <ImageUploader label="Select from Computer" onImageChangeHandler={onImageChangeHandler} />
+      {images.length > 0  && (
+        <ul className={classes.images}>
+          {images.map(({ src }) => (
+            <li key={src} className={classes.images__image}>
+              <img key={src} src={src} alt="" />
+              <Button className={classes.images__image__close} onClick={deleteImageHandler} value={src}>X</Button>
+            </li>
+          ))}
+        </ul>
+        )}
       <LoremIpsumPlaceholder />
       {/*<Modal open title="Add Photo" onClose={() => {}}/>*/}
     </div>
--- src/shared/lib/imageStore.ts
+++ src/shared/lib/imageStore.ts
@@ -2,27 +2,41 @@ import { createSlice, current } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 
 export type ImageStoreStateType = {
-  images: Array<{}>
+  images: Array<ImageType>
+  error: string
+}
+
+export type ImageType = {
+    src: string
+    type: string
+    name: string
+    size: number
 }
 
 const initialState: ImageStoreStateType = {
-  images: [
-
-  ],
+  images: [],
+  error: ''
 }
 
 export const imageSlice = createSlice({
   name: 'image',
   initialState,
   reducers: {
-    addImage:(state,  action: PayloadAction<{ image:string}> )=> {
+    addImage: (state,  action: PayloadAction<ImageType>) => {
+     if (state.images.length < 8) {
+       console.log('STATE - ', current(state))
         state.images.push(action.payload) 
         console.log(current(state))
+        if (state.error) state.error = '';
+      } else {
+        state.error = 'Error'
+      }
     },
-    removeImage(state, action: PayloadAction<{ index: number }>) {
-        state.images.filter((i)=>i !== action.payload.index)
-    },
-    
+
+    removeImage: (state, action: PayloadAction<{ src: string }>) => {
+        const filteredImages = state.images.filter(({src}) => src != action.payload.src);
+        state.images = filteredImages;
+    }
   },
 })
 
