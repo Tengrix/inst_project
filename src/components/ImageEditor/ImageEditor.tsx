741b6673f920686678795f7896687c7554538127
alexsharich <alexsharich@gmail.com>
2023-08-08 10:29:47 +0200

    feat: add ImageEditor component

---
+++ src/components/ImageEditor/ImageEditor.tsx
@@ -0,0 +1,36 @@
+import { ImageGalleryUploader } from '@/shared/ui/file-uploader/file-uploader';
+import { useAppSelector } from '@/store';
+import classes from './ImageEditor.module.scss';
+import { ImageManager } from '../ImageManager/ImageManager';
+import { addImage } from '@/shared/lib/imageStore';
+import { Dispatch } from 'react';
+import { useDispatch } from 'react-redux';
+
+export const ImageEditor = () => {
+  const isShowGallery = useAppSelector((state) => state.images.isShowGallery);
+  const dispatch = useDispatch();
+  const icons = [
+    { iconTitle: 'resize', className: 'icon_expand' },
+    { iconTitle: 'scale', className: 'icon_maximizeOutline' },
+    { iconTitle: 'gallery', className: 'icon_imageOutline' },
+  ];
+  const onImageChangeHandler = (event: any) => {
+    if (event.target.files && event.target.files[0]) {
+      const { name, size, type } = event.target.files[0];
+      const src = URL.createObjectURL(event.target.files[0]);
+
+      dispatch(addImage({ name, size, type, src }));
+    }
+  };
+
+  return (
+    <div className={classes.currentImageField}>
+      {isShowGallery && (
+        <div className={classes.imageGalleryUploader}>
+          <ImageGalleryUploader label="gallery" onImageChangeHandler={onImageChangeHandler} />
+        </div>
+      )}
+      <ImageManager icons={icons} />
+    </div>
+  );
+};
