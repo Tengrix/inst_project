b3e9bc4b099f4c7bdf77995cc958b5e5ba9e5649
pavlik <readystores.ru@gmail.com>
2023-08-09 21:10:23 +0300

    refactor: change navbar usestate

--- src/components/ImageEditor/Navbar/ImageNavbar.tsx
+++ src/components/ImageEditor/Navbar/ImageNavbar.tsx
@@ -1,9 +1,8 @@
-import { useDispatch } from 'react-redux';
-import s from './ImageNavbar.module.scss';
 import { ReactNode, useState } from 'react';
-import { ImageResize } from '../NavbarItems/Resize/ImageResize';
 import { ImageGallery } from '../NavbarItems/Gallery/ImageGallery';
+import { ImageResize } from '../NavbarItems/Resize/ImageResize';
 import { ImageScale } from '../NavbarItems/Scale/ImageScale';
+import s from './ImageNavbar.module.scss';
 
 type ImageNavbarPropsType = {
   callback?: () => void;
@@ -15,46 +14,52 @@ type IconType = {
 };
 
 export const ImageNavbar = ({ callback }: ImageNavbarPropsType) => {
-  const dispatch = useDispatch();
   const [items, setItems] = useState([
-    { iconTitle: 'resize', className: 'icon_expand', children: <ImageResize />, isShow: false },
+    { iconTitle: 'resize',
+      className: 'icon_expand',
+      children: <ImageResize />
+      //isShow: false 
+    },
     {
       iconTitle: 'scale',
       className: 'icon_maximizeOutline',
-      children: <ImageScale />,
-      isShow: false,
+      children: <ImageScale />
+      //isShow: false,
     },
     {
       iconTitle: 'gallery',
       className: 'icon_imageOutline',
-      children: <ImageGallery />,
-      isShow: false,
+      children: <ImageGallery />
+      //isShow: false,
     },
   ]);
-  const showItem = (value: string) => {
-    const newItems = items.map((item) => {
+
+  const [isShow, setShow] = useState(false);
+  const [comp, setComp] = useState();
+
+  const showItem = (component: any) => {
+    /*const newItems = items.map((item) => {
       return item.iconTitle === value
         ? { ...item, isShow: !item.isShow }
         : { ...item, isShow: false };
     });
     setItems(newItems);
+    */
+    setShow(!isShow);
+    setComp(component);
   };
 
   return (
-    <>
       <div className={s.currentImageManager}>
+        {isShow && comp}
         {items.map((item) => (
-          <div>
-            {item.isShow && item.children}
             <button
               key={item.iconTitle}
               className={s.imageManagerButton + ' ' + item.className}
-              onClick={(e: any) => showItem(e.target.value)}
+              onClick={() => showItem(item.children)}
               value={item.iconTitle}
             ></button>
-          </div>
         ))}
       </div>
-    </>
   );
 };
