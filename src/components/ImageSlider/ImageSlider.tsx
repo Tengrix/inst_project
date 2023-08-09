383eef9d4206a590334ed782fd33c9d1d336a5bf
pavlik <readystores.ru@gmail.com>
2023-08-09 11:53:08 +0300

    feat(component): add styles, bullets

--- src/components/ImageSlider/ImageSlider.tsx
+++ src/components/ImageSlider/ImageSlider.tsx
@@ -1,4 +1,6 @@
 import { ImageType } from "@/shared/lib/imageStore";
+import s from "./ImagesSlider.module.scss";
+import Link from "next/link";
 
 
 type ImageSliderPropsType = {
@@ -8,18 +10,33 @@ type ImageSliderPropsType = {
 
 export const ImageSlider = ({currentImage, images} : ImageSliderPropsType) => {
 
+    //const getHash = (src: string) => (src.match(/(?=\/([a-z-0-9]+)$)/) || [])[1] ?? src;
+    const getHash = (src: string) => src.replace(/^.*\//, '');
+
     return (
-        <div className="slider">
+        <div className={s.slider}>
         {images.length > 0 && (
-                <ul>
+                <ul className={s.slider__list}>
                     {images.map(({ src }) => (
-                        <li key={src}>
-                            <img src={src} alt=""/>
+                        <li id={getHash(src)} key={src} className={s.slider__item}>
+                            <img className={s.slider__image} src={src} alt=""/>
                         </li>
                     ))}
                 </ul>
             )
         }
+
+        {images.length > 0 && (
+            <ul className={s.bullets}> 
+                {images.map(({ src }) => (
+                    <li key={getHash(src)} className={s.bullets__item}>
+                        <Link  href={`#${getHash(src)}`}></Link>
+                    </li>
+                    )
+                )}
+            </ul>
+        )}
+
         </div>
     )
 }
