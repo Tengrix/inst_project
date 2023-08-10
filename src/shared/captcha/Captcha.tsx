191f3dda273840f8ddda98dcd9d85dd1af6fc221
Evgeny94 <lucky10291994@gmail.com>
2023-08-10 17:46:03 +0300

    fix: captcha refactored

--- src/shared/captcha/Captcha.tsx
+++ src/shared/captcha/Captcha.tsx
@@ -1,20 +1,15 @@
-import { useState } from 'react';
 import ReCAPTCHA from 'react-google-recaptcha';
 import s from './Captcha.module.scss';
 
 type CaptchaPropsType = {
-  changeCaptchaValue: (captchIsDone: boolean) => void;
   setCaptchaValue: (value:string)=>void
 };
 
-export const Captcha = ({ changeCaptchaValue,setCaptchaValue }: CaptchaPropsType) => {
-  const [captchIsDone, setCaptchaIsDone] = useState(false);
+export const Captcha = ({ setCaptchaValue }: CaptchaPropsType) => {
   const key = process.env.NEXT_PUBLIC_RECAPTCHA_API_KEY as string;
 
   const onChange = (value:string|null) => {
     value&&setCaptchaValue(value)
-    setCaptchaIsDone(true);
-    changeCaptchaValue(captchIsDone);
   };
 
   return (
