cdda9b50c52a1e71beb887e1c5c384be0e8e1e66
Evgeny94 <lucky10291994@gmail.com>
2023-08-11 18:23:52 +0300

    Revert "query"
    This reverts commit 78d2bfb0ef7d94a85467fbd5dc7a3eebf7a72989.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,41 +1,16 @@
-// import {configureStore} from "@reduxjs/toolkit";
-// import {TypedUseSelectorHook, useSelector} from "react-redux";
-// import {authApi} from "src/api/authApi";
-
-// import { userApi } from 'src/api/generalApi';
-
-// export const store = configureStore({
-//     reducer: {
-//         [authApi.reducerPath]: authApi.reducer,
-//     },
-//     middleware: getDefaultMiddleware => getDefaultMiddleware().concat(authApi.middleware)
-// })
-
-
-// export type RootStateType = ReturnType<typeof store.getState>
-
-// export const useAppSelector: TypedUseSelectorHook<RootStateType> = useSelector
-// export type AppDispatch = typeof store.dispatch
-
-
-
-
-import { configureStore } from "@reduxjs/toolkit";
-import { TypedUseSelectorHook, useSelector } from "react-redux";
-import { authApi } from "src/api/authApi";
-import { userApi } from "src/api/generalApi";
+import {configureStore} from "@reduxjs/toolkit";
+import {TypedUseSelectorHook, useSelector} from "react-redux";
+import {authApi} from "src/api/authApi";
 
 export const store = configureStore({
-  reducer: {
-    [authApi.reducerPath]: authApi.reducer,
-    [userApi.reducerPath]: userApi.reducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware()
-      .concat(authApi.middleware)
-      .concat(userApi.middleware),
-});
+    reducer: {
+        [authApi.reducerPath]: authApi.reducer,
+    },
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(authApi.middleware)
+})
 
-export type RootStateType = ReturnType<typeof store.getState>;
-export const useAppSelector: TypedUseSelectorHook<RootStateType> = useSelector;
-export type AppDispatch = typeof store.dispatch;
+
+export type RootStateType = ReturnType<typeof store.getState>
+
+export const useAppSelector: TypedUseSelectorHook<RootStateType> = useSelector
+export type AppDispatch = typeof store.dispatch
