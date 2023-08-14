8ecfc7153df29a1769432b95f100cd0074ea212d
alexsharich <alexsharich@gmail.com>
2023-08-14 18:28:55 +0200

    feat: add filters for canvas

--- src/components/ImageEditor/NavbarItems/Filter/ImageFilter.tsx
+++ src/components/ImageEditor/NavbarItems/Filter/ImageFilter.tsx
@@ -1,55 +1,44 @@
 import { useAppDispatch } from '@/store';
 import { MouseEvent } from 'react';
 import s from './ImageFilter.module.scss';
+import { createNewImageBlob } from '@/shared/lib/imageStore';
 
+export const ImageFilter = ({ image }) => {
+  const dispatch = useAppDispatch();
+  const filters = [
+    { className: 'blur', value: 'blur(4px)' },
+    { className: 'brightness', value: 'brightness(250%' },
+    { className: 'contrast', value: 'contrast(180%)' },
+    { className: 'grayscale', value: 'grayscale(100%)' },
+    { className: 'huerotate', value: 'hue-rotate(180deg)' },
+    { className: 'invert', value: 'invert(100%)' },
+    { className: 'opacity', value: 'opacity(50%)' },
+    { className: 'saturate', value: 'saturate(7)' },
+    { className: 'sepia', value: 'sepia(100%)' },
+  ];
 
-export const ImageFilter = ({image}) => {
-    const dispatch = useAppDispatch();
-
-    const applyFilterForImageHandler = (e: MouseEvent<HTMLButtonElement>) => {
-        if (e.currentTarget.value) {
-            console.log(e.currentTarget.value)
-            /*dispatch(
-              addFilterToCurrentImage({ 
-                filterName: 'crop',
-                args: e.currentTarget.value 
-              }
-            ));*/
-            dispatch(createNewImageBlob({ filterName: 'crop', args: e.currentTarget.value }));
-        }
+  const applyFilterForImageHandler = (/* e: MouseEvent<HTMLButtonElement> */ filter: string) => {
+    if (filter) {
+      dispatch(
+        createNewImageBlob({ filterName: 'color', /* args: e.currentTarget.value */ args: filter }),
+      );
     }
+  };
 
-    return (
-        <div className={s.container}>
-            <ul className={s.filters}>
-                <li className={s.filter}>
-                    <img className={s.blur} src={image.src} alt="" />
-                </li>
-                <li className={s.filter}>
-                    <img className={s.brightness} src={image.src} alt="" />
-                </li>
-                <li className={s.filter}>
-                    <img className={s.contrast} src={image.src} alt="" />
-                </li>
-                <li className={s.filter}>
-                    <img className={s.grayscale} src={image.src} alt="" />
-                </li>
-                <li className={s.filter}>
-                    <img className={s.huerotate} src={image.src} alt="" />
-                </li>
-                <li className={s.filter}>
-                    <img className={s.invert} src={image.src} alt="" />
-                </li>
-                <li className={s.filter}>
-                    <img className={s.opacity} src={image.src} alt="" />
-                </li>
-                <li className={s.filter}>
-                    <img className={s.saturate} src={image.src} alt="" />
-                </li>
-                <li className={s.filter}>
-                    <img className={s.sepia} src={image.src} alt="" />
-                </li>
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div className={s.container}>
+      <ul className={s.filters}>
+        {filters.map((filter) => (
+          <li className={s.filter}>
+            <img
+              className={s[`${filter.className}`]}
+              src={image.src}
+              alt={filter.className}
+              onClick={() => applyFilterForImageHandler(filter.value)}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
