5716f7e08f361fb94adcbcdbc7833cbdffcc5165
Evgeny94 <lucky10291994@gmail.com>
2023-08-16 11:51:17 +0300

    fix: CreatePostModal.tsx refactored

--- src/components/ImageEditor/ImageEditor.tsx
+++ src/components/ImageEditor/ImageEditor.tsx
@@ -4,29 +4,33 @@ import { ImageSlider } from '../ImageSlider/ImageSlider';
 import classes from './ImageEditor.module.scss';
 import { ImageNavbar } from './Navbar/ImageNavbar';
 import { ImageFilter } from './NavbarItems/Filter/ImageFilter';
+import {StepType} from "@/pages/post/createPostModal/CreatePostModal";
 
 type ImageEditorPropsType = {
   image: string;
+  step: StepType
 };
 
-export const ImageEditor = ({ image }: ImageEditorPropsType) => {
+export const ImageEditor = (props:ImageEditorPropsType) => {
   const currentImage = useAppSelector((state) => state.images.currentImage) || '';
   const images = useAppSelector((state) => state.images.images);
 
   return (
-    <>
+    <div className={classes.container}>
       <div className={classes.currentImageField}>
         <ImageSlider currImage={currentImage} images={images} />
-        <ImageNavbar image={currentImage} />
+          {props.step === 'Cropping' && <ImageNavbar/>}
       </div>
-      <div className={classes.container}>
-        <div className={classes.slider}>
-          <ImageSlider currImage={currentImage} images={images} />
-        </div>
-        <div className={classes.filters}>
-          <ImageFilter image={currentImage} />
-        </div>
-      </div>
-    </>
+        {props.step === 'Filters' &&
+            <div className={classes.right}>
+                <ImageFilter image={currentImage}/>
+            </div>
+        }
+        {props.step === 'Publication' &&
+            <div className={classes.right}>
+                DESCRIPTION BLOCK
+            </div>
+        }
+    </div>
   );
 };
