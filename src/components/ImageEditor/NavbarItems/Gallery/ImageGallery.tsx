e23c8109e82e1bbf8b55c9c75636c9e1423002c6
alexsharich <alexsharich@gmail.com>
2023-08-23 11:13:10 +0200

    refactor: code refactor with eslint, prettier rules

--- src/components/ImageEditor/NavbarItems/Gallery/ImageGallery.tsx
+++ src/components/ImageEditor/NavbarItems/Gallery/ImageGallery.tsx
@@ -1,53 +1,50 @@
-import { addImage, removeImage, setCurrentImage } from '@/redux/store/imageSlice/imageSlice';
-import { Button } from '@/shared/ui/button';
-import { ImageUploader } from '@/shared/ui/file-uploader/file-uploader';
+import s from '@/components/ImageEditor/NavbarItems/Gallery/ImageGallery.module.scss';
 import { useAppDispatch, useAppSelector } from '@/redux/store';
-import classes from '@/components/ImageEditor/NavbarItems/Gallery/ImageGallery.module.scss';
-import {parseImageBlob} from "@/shared/utils/parseImageBlob";
+import { addImage, removeImage, setCurrentImage } from '@/redux/store/imageSlice/imageSlice';
+import { ImageUploader } from '@/shared/ui/file-uploader/file-uploader';
+import { parseImageBlob } from '@/shared/utils/parseImageBlob';
 
 export const ImageGallery = () => {
-  const dispatch = useAppDispatch();
-  const images = useAppSelector((state) => state.images.images);
+    const dispatch = useAppDispatch();
+    const images = useAppSelector(state => state.images.images);
 
-  const addImageToGallery = (event: any) => {
-    if (event.target.files && event.target.files[0]) {
-      const blob = event.target.files[0];
-      const image = parseImageBlob(blob);
-      dispatch(addImage(image));
-    }
-  };
+    const addImageToGallery = (event: any) => {
+        if (event.target.files && event.target.files[0]) {
+            const blob = event.target.files[0];
+            const image = parseImageBlob(blob);
+            dispatch(addImage(image));
+        }
+    };
 
-  const removeImageFromGallery = (e: any) => {
-    if (e.currentTarget.value) {
-      dispatch(removeImage({ src: e.currentTarget.value }));
-    }
-  };
+    const removeImageFromGallery = (e: any) => {
+        if (e.currentTarget.value) {
+            dispatch(removeImage({ src: e.currentTarget.value }));
+        }
+    };
 
-  const imagePreview = images.map(({ originalSRC }) => (
-    <li key={originalSRC} className={classes.image}>
-      <img
-        src={originalSRC}
-        alt=""
-        onClick={() => {
-          dispatch(setCurrentImage(originalSRC));
-        }}
-      />
-      <button
-        className={classes.image_close}
-        onClick={removeImageFromGallery}
-        value={originalSRC}
-      >
-        <span className={classes.icon__close}></span>
-      </button>
-    </li>
-  ));
+    const imagePreview = images.map(({ originalSRC }) => (
+        <li key={originalSRC} className={s.image}>
+            <div
+                role="button"
+                aria-hidden
+                onClick={() => {
+                    dispatch(setCurrentImage(originalSRC));
+                }}>
+                <img src={originalSRC} alt="" />
+            </div>
 
-  return (
-    <div className={classes.imageGalleryUploader}>
-      <div className={classes.galleryContainer}>
-        {images.length > 0 && <ul className={classes.images}>{imagePreview}</ul>}
-        <ImageUploader label={'+'} onImageChangeHandler={addImageToGallery} />
-      </div>
-    </div>
-  );
+            <button className={s.image_close} onClick={removeImageFromGallery} value={originalSRC}>
+                <span className={s.icon__close}></span>
+            </button>
+        </li>
+    ));
+
+    return (
+        <div className={s.imageGalleryUploader}>
+            <div className={s.galleryContainer}>
+                {images.length > 0 && <ul className={s.images}>{imagePreview}</ul>}
+                <ImageUploader label={'+'} onImageChangeHandler={addImageToGallery} />
+            </div>
+        </div>
+    );
 };
