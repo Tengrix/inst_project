2cb8c6b88d9931f91ba181628d16aff3f403b9b9
Tengrix <damir861@gmail.com>
2023-09-02 19:39:27 -0400

    PRC-35

--- src/components/Redirect/Redirect.tsx
+++ src/components/Redirect/Redirect.tsx
@@ -1,34 +1,37 @@
 import React, {FC, PropsWithChildren, useEffect, useState} from 'react';
 import Spinner from "@/shared/ui/spinner/Spinner";
-import {useLazyRefreshTokenQuery, useRefreshTokenQuery} from "@/redux/store/Auth/authApiSlice";
+import {useRefreshTokenQuery} from "@/redux/store/Auth/authApiSlice";
 import {useAppDispatch, useAppSelector} from "@/redux/store";
 import {authAction} from "@/redux/store/Auth/authSlice";
 import {useRouter} from "next/router";
-import {Routes} from "@/shared/routes/Routes";
+import {PrivateRoutes, PrivateRoutesType, PublicRoutes, PublicRoutesType, Routes} from "@/shared/routes/Routes";
 
 export const Redirect: FC<PropsWithChildren> = ({children}) => {
     const {data,isSuccess,isError} = useRefreshTokenQuery()
     const [isLoading,setIsLoading] = useState(true)
     const {token,trustDevice} = useAppSelector(state=> state.auth)
     const dispatch = useAppDispatch()
-    const {push} = useRouter()
+    const {push,pathname} = useRouter()
 
     useEffect(() => {
         if(isSuccess){
             dispatch(authAction.setCredentials(data))
+            if(PrivateRoutes.includes(pathname as  PrivateRoutesType)){
+                push(pathname).then(()=>setIsLoading(false))
+            }
             setIsLoading(false)
         }
     }, [data])
 
     useEffect(()=>{
         if(isError){
-            push(Routes.LOGIN).then(()=>setIsLoading(false))
-            setIsLoading(false)
+            if(PublicRoutes.includes(pathname as PublicRoutesType)){
+                push(pathname).then(()=>setIsLoading(false))
+            }else{
+                push(Routes.LOGIN).then(()=>setIsLoading(false))
+            }
         }
-    },[isError])
-
-
-    console.log('REDIRECT', {token})
+    },[isError,data])
 
     return (
         <>
--- src/shared/routes/Routes.ts
+++ src/shared/routes/Routes.ts
@@ -1,14 +1,18 @@
+export type PrivateRoutesType = '/home'|'/profile'|'/profile-settings'
+export type PublicRoutesType = '/sign-in'|'/sign-up'|'/forgot-password'
 export const Routes = {
     LOGIN: '/sign-in',
     REGISTER: '/sign-up',
+    FORGOT_PASSWORD:'/forgot-password',
     HOME: '/home',
     PROFILE: '/profile',
-    PROFILE_SETTINGS: '/profile/settings/',
+    PROFILE_SETTINGS: '/profile-settings',
 } as const
 
 export const PublicRoutes = [
     Routes.LOGIN,
     Routes.REGISTER,
+    Routes.FORGOT_PASSWORD
 ] as const
 export const PrivateRoutes = [
     Routes.PROFILE,
