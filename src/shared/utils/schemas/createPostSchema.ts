dcda8ae6b60342cdb3e6a6aaaeb609a239321963
alexsharich <alexsharich@gmail.com>
2023-09-09 15:17:24 +0200

    fix: fix i18n for imageEditor, add i18n for messages, change createPostSchema

--- src/shared/utils/schemas/createPostSchema.ts
+++ src/shared/utils/schemas/createPostSchema.ts
@@ -4,13 +4,13 @@ export const createPostSchema = z.object({
     title: z
         .string({ required_error: 'error.descriptionIsRequiredError' })
         .trim()
-        .min(8, 'error.titleValueMin')
-        .max(30, 'error.titleValueMax'),
+        .min(8, 'error.titlePostValueMin')
+        .max(30, 'error.titlePostValueMax'),
     description: z
         .string({ required_error: 'error.descriptionIsRequiredError' })
         .trim()
-        .min(10, 'error.descriptionValueMin')
-        .max(500, 'error.descriptionValueMax')
+        .min(10, 'error.descriptionPostValueMin')
+        .max(500, 'error.descriptionPostValueMax')
 });
 
 export type CreatePostFormType = z.infer<typeof createPostSchema>;
