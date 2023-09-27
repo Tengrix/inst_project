2b196eb1a9bea154d79716c1d8ebf55a90b91977
Evgeny94 <lucky10291994@gmail.com>
2023-09-27 13:55:17 +0300

    fix: Redirect refactor

--- src/components/Redirect/Redirect.tsx
+++ src/components/Redirect/Redirect.tsx
@@ -8,7 +8,12 @@ import { PrivateRoutes, PublicRoutes, Routes } from '@/shared/routes/Routes';
 import Spinner from '@/shared/ui/spinner/Spinner';
 
 export const Redirect: FC<PropsWithChildren> = ({ children }) => {
-    const { data, isSuccess, isError } = useRefreshTokenQuery(undefined, { pollingInterval: 5 * 60 * 1000 });
+    const {
+        data,
+        isSuccess,
+        isError,
+        isLoading: refreshTokenIsLoading
+    } = useRefreshTokenQuery(undefined, { pollingInterval: 5 * 60 * 1000, refetchOnMountOrArgChange: true });
     const [isLoading, setIsLoading] = useState(true);
     const dispatch = useAppDispatch();
     const { push, pathname } = useRouter();
@@ -21,34 +26,36 @@ export const Redirect: FC<PropsWithChildren> = ({ children }) => {
         return PrivateRoutes.some(route => path.startsWith(route));
     };
 
-    useEffect(() => {
-        if (isSuccess) {
-            dispatch(authAction.setCredentials(data));
-            setIsLoading(false);
-        }
-    }, [data]);
-    useEffect(() => {
-        if (isPublicRoute(pathname)) {
-            setIsLoading(false);
-        } else {
-            isError && push(Routes.LOGIN).then(() => setIsLoading(false));
-        }
-    }, [isError, data]);
-
     // useEffect(() => {
-    //     setIsLoading(true);
-    //     debugger
+    //     if (isSuccess) {
+    //         dispatch(authAction.setCredentials(data));
+    //         setIsLoading(false);
+    //     }
+    // }, [data]);
+    // useEffect(() => {
     //     if (isPublicRoute(pathname)) {
     //         setIsLoading(false);
     //     } else {
-    //         if (isSuccess) {
-    //             dispatch(authAction.setCredentials(data));
-    //             setIsLoading(false);
-    //         } else {
-    //             push(Routes.LOGIN).then(() => setIsLoading(false));
-    //         }
+    //         isError && push(Routes.LOGIN).then(() => setIsLoading(false));
     //     }
-    // }, [data]);
+    // }, [isError, data]);
+
+    useEffect(() => {
+        if (refreshTokenIsLoading) {
+            setIsLoading(true);
+        } else {
+            if (isPublicRoute(pathname)) {
+                setIsLoading(false);
+            } else {
+                if (isSuccess) {
+                    dispatch(authAction.setCredentials(data));
+                    setIsLoading(false);
+                } else {
+                    push(Routes.LOGIN).then(() => setIsLoading(false));
+                }
+            }
+        }
+    }, [data, refreshTokenIsLoading, dispatch, pathname, isSuccess, push]);
 
     return <>{isLoading ? <Spinner /> : children}</>;
 };
