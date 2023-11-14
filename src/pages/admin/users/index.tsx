32e2bae67ff9182f48fd8d441f4ab6d78192fe49
Evgeny94 <lucky10291994@gmail.com>
2023-11-14 13:33:17 +0300

    refactor: destructure graphql queries

--- src/pages/admin/users/index.tsx
+++ src/pages/admin/users/index.tsx
@@ -1,7 +1,8 @@
-import { gql, useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 
 import { getLayoutAdmin } from '@/components/Layout/AdminLayout/AdminLayout';
 import Table from '@/components/Table/Table';
+import { USERS } from '@/pages/admin/queries/users';
 import Spinner from '@/shared/ui/spinner/Spinner';
 
 const header = {
@@ -10,22 +11,6 @@ const header = {
     role: 'Role',
     email: 'Email'
 };
-
-const USERS = gql`
-    query getAllUsers(
-        $userId: String! = "ae393edd-a455-4015-baf6-a11dc389172b"
-        $page: Float! = 1
-        $itemsPerPage: Float! = 32
-        $search: String! = ""
-    ) {
-        getAllUsers(userId: $userId, page: $page, itemsPerPage: $itemsPerPage, search: $search) {
-            id
-            login
-            role
-            email
-        }
-    }
-`;
 
 const UsersList = () => {
     const { loading, error, data } = useQuery(USERS);
