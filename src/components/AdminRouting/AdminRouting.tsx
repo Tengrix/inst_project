86509e06653754a74f9560948e6b8b73fcc2c8a9
alexsharich <alexsharich@gmail.com>
2023-11-16 21:15:45 +0100

    fix: fix admin sidebar icons

--- src/components/AdminRouting/AdminRouting.tsx
+++ src/components/AdminRouting/AdminRouting.tsx
@@ -1,19 +1,23 @@
 import { HomeIcon } from '@radix-ui/react-icons';
 
+import { PaymentsIcon } from '@/assets/icons/PaymentsIcon';
+import { PostsIcon } from '@/assets/icons/PostsIcon';
+import { StatisticsIcon } from '@/assets/icons/StatisticsIcon co';
+import { UsersIcon } from '@/assets/icons/UsersIcon';
 import Sidebar from '@/components/Sidebar/Sidebar';
 import { SidebarRouteType } from '@/components/UserRouting/UserRouting';
 type SidebarRoutesTypes = Array<SidebarRouteType>;
 
 export const AdminRouting = () => {
     const adminRoutes: SidebarRoutesTypes = [
-        { title: 'usersList', icon: <HomeIcon height={60} width={24} />, path: '/users-list' },
+        { title: 'usersList', icon: <UsersIcon height={60} width={24} />, path: '/users-list' },
         {
             title: 'statistics',
-            icon: <HomeIcon height={60} width={24} />,
+            icon: <StatisticsIcon height={60} width={24} />,
             path: '/statistics'
         },
-        { title: 'paymentsList', icon: <HomeIcon height={60} width={24} />, path: '/payments-list' },
-        { title: 'postsList', icon: <HomeIcon height={60} width={24} />, path: '/posts-list' }
+        { title: 'paymentsList', icon: <PaymentsIcon height={60} width={24} />, path: '/payments-list' },
+        { title: 'postsList', icon: <PostsIcon height={60} width={24} />, path: '/posts-list' }
     ];
 
     return (
